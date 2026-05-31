ec319921cba47b9e3cb49ff5659c4bb3c7e01285
Mikael Ødegaard <mikael.kodehode@gmail.com>
2026-05-31 09:50:52 +0200

    fixed  some bugs, improved profiles and added iyouxin

--- hera-invitational-rankings/src/shared/profiles/grubby.ts
+++ hera-invitational-rankings/src/shared/profiles/grubby.ts
@@ -1,17 +1,23 @@
 export const grubbyProfileInfo = `
   <p>
-    Manuel "Grubby" Schenkhuizen (40) is an ex-professional Dutch Warcraft 3 Orc player from Nieuwegein, Netherlands. He is currently a variety streamer with Warcraft 3 as a main fallback.
+    Manuel "Grubby" Schenkhuizen is an ex-professional Dutch Warcraft 3 Orc player from Nieuwegein, Netherlands. He is a successful variety streamer with 
+    Warcraft 3 as part of the core identity and recurring program. He holds very popular successful tournaments in Warcraft 3 and the whole world rejoice every 
+    time he announces one &#128079&#128079&#128079. Warcraft is the foundation of Grubby's character and development as a person. A person once said "To watch or 
+    not to watch Grubby's stream. That's a question."
+    <br/><br/>
+    His popularity comes down to him being an extremely friendly and likable person with the courage of stepping outside his main domain. 
   </p>
 `
 
 export const grubbyTrivia = `
   <ul>
-    <li>His name comes from when he played Dungeons and Dragons with his older brothers. Originally "Grubtor Steelheart", he was given the nickname "Grubby" when playing with others over the internet, later he kept the nickname replacing his original. At the time he wasn't aware of the English meaning of the word and claims to be unsure as to whether he would have kept it had he known at the time.</li>
-    <li>He played Protoss in StarCraft 2.</li>
-    <li>He is of Indonesian descent.</li>
-    <li>He won Electronic Sports League Warcraft 3 MVP 4 times.</li>
-    <li>He has won more than 38 LAN tournaments, 6 World Championships</li>
-    <li>Favorite in-game color: Green.</li>
+    <li>His name comes from when he played Dungeons and Dragons with his older brothers. Originally "Grubtor Steelheart", he was given the nickname "Grubby" when 
+    playing with others over the internet, later he kept the nickname replacing his original. At the time he wasn't aware of the English meaning of the word and claims 
+    to be unsure as to whether he would have kept it had he known at the time.</li>
+    <li>Together with Lyn he is considered the best orc player of all time.</li>
+    <li>He is of Indonesian/Dutch descent.</li>
+    <li>He is a pro-am farmer with a striking dedication towards his feathered friends and hunts in his garden from time to time.</li>
+    <li>He is a breadlover often eating bread alone as long as the bread is good.</li>
   </ul>
 
 `
---
+++ hera-invitational-rankings/src/shared/profiles/iyouxin.ts
@@ -0,0 +1,17 @@
+export const iyouxinProfileInfo = `
+  <p>
+    Gena (iyouxin) is a Ukranian Twitch streamer known for good vibes playing high level World of Tanks. Together with Gunnar they are the children in the competition 
+    compared to the field sitting at 26 years old. Hold on to your reading glasses and suspenders because these are some wild ones! As it stands they have the least games, 
+    but that just means the competition won't know what hit them with a Hoang or the lethal Phosphorus.  
+    <br/><br/>
+    His aggressive style with high energy in World of Tanks might influence his AoE style of play. 
+  </p>
+`
+
+export const iyouxinTrivia = `
+  <ul>
+    <li>His first video on Youtube was about the Bat.-Châtillon 25 t AP. It weighs 25 tons.</li>
+    <li>He lives in Germany.</li>
+  </ul>
+
+`
